6570f721e70621eabeab4af2aae2f0483bf5f240
Christian Schneeweiss <Schneeweiss97@gmail.com>
2023-08-24 22:14:49 +0200

    added private procedure with isAuth middleware and pretteir

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,6 +1,19 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 
 const t = initTRPC.create();
 
+const middleware = t.middleware;
+
+const isAuth = middleware(async (opts) => {
+  const user = { id: "asdf" }; // TODO get user from authentication
+  if (!user) throw new TRPCError({ code: "UNAUTHORIZED" });
+  return opts.next({
+    ctx: {
+      user,
+    },
+  });
+});
+
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const privateProcedure = publicProcedure.use(isAuth);
